0679f47d897d7bbbf2e71d1a5effeac1f6651459
sounair <sounair@gmail.com>
2022-03-08 18:16:37 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "name" {
+    default="mytf2"
+}
+variable"location"{
+    default="eastus"
+}
+variable "sid"{
+    default="b791e0a8-9e09-40ba-bbf0-21e6e29d2e67"
+}
+variable "cid"{
+    default="84bee442-df98-40cf-8374-c7a18c8d194a"
+}
+variable "tenantid"{
+    default="ac312fcc-3092-4729-82a8-2d6a19d0f129"
+}
