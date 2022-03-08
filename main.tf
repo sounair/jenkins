4fb8af4aca658245ace772c1646d686d1395a21c
sounair <sounair@gmail.com>
2022-03-08 20:15:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
-provider "azurerm" {
 
-
+variable "clients" {
+  type        = string
+  description = "clientsecret from jenkins"
+}
+  provider "azurerm" {
   subscription_id = "${var.sid}"
   client_id       = "${var.cid}"
-  client_secret   = $clients
+  client_secret   = var.clients
   tenant_id       = "${var.tenantid}"
   features {}
 }
