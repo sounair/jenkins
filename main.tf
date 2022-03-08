df01c96141c0f40d8cdaf34140be38d61f7e238e
sounair <sounair@gmail.com>
2022-03-08 17:33:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+
+
+  subscription_id = "${var.sid}"
+  client_id       = "${var.cid}"
+  client_secret   = "${var.csecret}"
+  tenant_id       = "${var.tenantid}"
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.name}-resource"
+  location = "${var.location}"
+}
